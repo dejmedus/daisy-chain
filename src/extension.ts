855ccae204586015ca82cb8b1843d110c82b7a3a
dejmedus <email96123@gmail.com>
2023-01-31 12:28:42 -0700

    add Remove daisy chain command

--- src/extension.ts
+++ src/extension.ts
@@ -11,16 +11,25 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("daisy-chain.start", () => {
+  let chains: vscode.StatusBarItem[] = [];
+
+  let add = vscode.commands.registerCommand("daisy-chain.start", () => {
     const statusBarItem: vscode.StatusBarItem =
       vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 400);
-
+    chains.push(statusBarItem);
     statusBarItem.text = `✿✿✿✿✿✿✿✿✿✿✿✿`;
 
     statusBarItem.show();
   });
 
-  context.subscriptions.push(disposable);
+  let remove = vscode.commands.registerCommand("daisy-chain.end", () => {
+    chains[chains.length - 1].dispose();
+    chains.pop();
+    console.log(chains);
+  });
+
+  context.subscriptions.push(add);
+  context.subscriptions.push(remove);
 }
 
 // This method is called when your extension is deactivated
