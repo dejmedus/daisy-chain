83e48eb09d6bc5df048333a526d22f15eec424b3
dejmedus <email96123@gmail.com>
2023-05-27 23:31:42 -0600

    remove uneeded comments

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,6 @@ import * as vscode from "vscode";
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "daisy-chain" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
